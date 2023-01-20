f129f6ee6791e9b3a4891054543b0dac18182b62
Emma_yxy <55633956+x67yan@users.noreply.github.com>
2023-01-20 00:55:27 -0500

    Update tours.py

--- graph/tours.py
+++ graph/tours.py
@@ -3,48 +3,6 @@ import graph
 import read_cities
 import path
 
-def paths_v(g,v):
-    '''
-    returns paths starting from v in g
-    
-    paths_v: (graph of X Y) -> (listof X)
-    '''
-    n = len(g.get_vertices())
-    paths = []
-    possible_path = []
-    joint_l = []
-    waiting = []
-    joint = {}
-    waiting.append(v)
-    while waiting != []:
-        processing = waiting.pop(0)
-        visit_check = processing in possible_path
-        if not visit_check:
-            possible_path.append(processing)
-        neighbours = g.get_edges(processing)
-        if len(neighbours) == 0 or visit_check:
-            if len(possible_path) == n and possible_path not in paths:
-                paths.append(possible_path)
-            if joint_l != []:
-                latest_joint = joint_l[-1]
-                waiting = [joint[latest_joint][0]]
-                pos = possible_path.index(latest_joint)
-                possible_path = possible_path[0: pos + 1]
-                joint[latest_joint].pop(0)
-                if joint[latest_joint] == []:
-                    joint.pop(latest_joint)
-                    joint_l.pop(-1)
-        elif len(neighbours) > 1:
-            joint[processing] = neighbours[1:]
-            joint_l.append(processing)
-            processing = neighbours[0]
-            waiting.append(processing)
-        else:
-            processing = neighbours[0]
-            waiting.append(processing)
-    return paths   
-
-
 def tours(g):
     '''
     consumes a Graph g and produces the list of paths which contain every vertex
